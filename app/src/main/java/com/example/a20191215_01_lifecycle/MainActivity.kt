4b0629048d443d9f76eada428cf7cc7c2bc7736e
hyun <rlaalgus132@naver.com>
2019-12-15 10:48:43 +0900

    백버튼 이벤트 커스터마이징

--- app/src/main/java/com/example/a20191215_01_lifecycle/MainActivity.kt
+++ app/src/main/java/com/example/a20191215_01_lifecycle/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.a20191215_01_lifecycle
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -16,7 +18,7 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValue() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,6 +29,17 @@ class MainActivity : BaseActivity() {
         setupEvent()
 
         Log.d("생명주기", "onCreate")
+    }
+
+    override fun onBackPressed() {
+//        super.onBackPressed()
+        val alert = AlertDialog.Builder(mContext)
+        alert.setTitle("게시글 작성 중지 확인")
+        alert.setMessage("정말 글 작성을 중단 하시겠습니까 ? 작성하신 내용은 모두 삭제됩니다.")
+        alert.setPositiveButton("확인",DialogInterface.OnClickListener { dialog, which -> finish() })
+        alert.setNegativeButton("취소",null)
+        alert.show()
+
     }
 
     override fun onResume() {
